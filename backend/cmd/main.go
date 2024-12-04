dcbdd0773d96c918b83c2ae4387397e8dfe35692
mrozek <01178582@pw.edu.pl>
2024-12-05 00:53:22 +0100

    IMPLEMENT: Implemented simple hello world in main

---
+++ backend/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"band-manager-backend/internal/db"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("BACKEND_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	db.InitDB()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// database := db.GetDB()
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	fmt.Printf("Server starting on http://localhost:%s\n", port)
+	if err := http.ListenAndServe(":" + port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
