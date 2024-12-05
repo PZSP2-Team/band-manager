140df9ee9797d446cfdd02d38b45addb14335628
mrozek <01178582@pw.edu.pl>
2024-12-05 21:50:55 +0100

    IMPLEMENT: Implemented notesheet model in GORM

---
+++ backend/internal/models/notesheet.go
@@ -0,0 +1,8 @@
+package models
+
+type Notesheet struct {
+    ID          	uint   			`gorm:"primarykey"`
+	Filepath		string			`gorm:"not null"`
+	TrackId     	uint   			`gorm:"not null;"`
+	Subgroups		[]*Subgroup		`gorm:"many2many:notesheet_subgroup;constraint:OnDelete:CASCADE"`
+}
