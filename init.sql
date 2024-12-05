ca3fb59214c06d26ebdee4c61df9cf6b0998bf6d
mrozek <01178582@pw.edu.pl>
2024-12-05 21:48:10 +0100

    FIX: Fixed minor issues with init.sql keeping it in repo for the purpose of reading how our database should look

--- init.sql
+++ init.sql
@@ -6,7 +6,7 @@ CREATE TABLE "groups" (
 
 CREATE TABLE "subgroups" (
     id SERIAL PRIMARY KEY,
-    group_id INTEGER REFERENCES "groups"(id) NOT NULL,
+    group_id INTEGER REFERENCES "groups"(id) ON DELETE CASCADE NOT NULL,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255)
 );
@@ -17,17 +17,17 @@ CREATE TABLE "users" (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    role VARCHAR(10) NOT NULL CHECK (role IN ('Default', 'Moderator', 'Manager')),
-    group_id INTEGER REFERENCES "groups"(id)
+    role VARCHAR(10) NOT NULL
+    group_id INTEGER REFERENCES "groups"(id) ON DELETE SET NULL
 );
 
 CREATE TABLE "announcements" (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    sender_id INTEGER REFERENCES "users"(id) NOT NULL, 
+    sender_id INTEGER REFERENCES "users"(id) ON DELETE SET NULL, 
     description VARCHAR(255) NOT NULL,
     priority INTEGER NOT NULL,
-    group_id INTEGER REFERENCES "groups"(id) NOT NULL
+    group_id INTEGER REFERENCES "groups"(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE "events" (
@@ -36,40 +36,41 @@ CREATE TABLE "events" (
     description VARCHAR(255),
     date DATE NOT NULL,
     location VARCHAR(255) NOT NULL,
-    group_id INTEGER REFERENCES "groups"(id) NOT NULL
+    group_id INTEGER REFERENCES "groups"(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE "tracks" (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    group_id INTEGER REFERENCES "groups"(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE "notesheets" (
     id SERIAL PRIMARY KEY,
-    track_id INTEGER REFERENCES "tracks"(id) NOT NULL,
+    track_id INTEGER REFERENCES "tracks"(id) ON DELETE CASCADE NOT NULL,
     filepath VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE "announcement_subgroup" (
-    announcement_id INTEGER REFERENCES "announcements"(id),
-    subgroup_id INTEGER REFERENCES "subgroups"(id),
+    announcement_id INTEGER REFERENCES "announcements"(id) ON DELETE CASCADE,
+    subgroup_id INTEGER REFERENCES "subgroups"(id) ON DELETE CASCADE,
     PRIMARY KEY (announcement_id, subgroup_id)
 );
 
 CREATE TABLE "subgroup_user" (
-    subgroup_id INTEGER REFERENCES "subgroups"(id),
-    user_id INTEGER REFERENCES "users"(id),
+    subgroup_id INTEGER REFERENCES "subgroups"(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES "users"(id) ON DELETE CASCADE,
     PRIMARY KEY (subgroup_id, user_id)
 );
 
 CREATE TABLE "notesheet_subgroup" (
-    notesheet_id INTEGER REFERENCES "notesheets"(id),
-    subgroup_id INTEGER REFERENCES "subgroups"(id),
+    notesheet_id INTEGER REFERENCES "notesheets"(id) ON DELETE CASCADE,
+    subgroup_id INTEGER REFERENCES "subgroups"(id) ON DELETE CASCADE,
     PRIMARY KEY (notesheet_id, subgroup_id)
 );
 
 CREATE TABLE "performances" (
-    event_id INTEGER REFERENCES "events"(id),
-    track_id INTEGER REFERENCES "tracks"(id),
+    event_id INTEGER REFERENCES "events"(id) ON DELETE CASCADE,
+    track_id INTEGER REFERENCES "tracks"(id) ON DELETE CASCADE,
     PRIMARY KEY (event_id, track_id)
 );
