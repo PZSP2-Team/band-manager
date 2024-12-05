eaef9fc26a5bad31c57de545fc3672efbd49d115
mrozek <01178582@pw.edu.pl>
2024-12-05 21:52:08 +0100

    IMPLEMENT: Implemented announcement model in GORM

---
+++ backend/internal/models/announcement.go
@@ -0,0 +1,10 @@
+package models
+
+type Announcement struct {
+	ID				uint 		`gorm:"primarykey"`
+	Title			string		`gorm:"not null"`
+	Description 	string		`gorm:"not null"`
+	Priority		uint		`gorm:"not null"`
+	GroupID			uint
+	SenderID		uint
+}
