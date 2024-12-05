e1e04df6c79f221aef7c112dfcd4ba34e2cdb0e3
mrozek <01178582@pw.edu.pl>
2024-12-05 21:51:03 +0100

    IMPLEMENT: Implemented group model in GORM

---
+++ backend/internal/models/group.go
@@ -0,0 +1,12 @@
+package models
+
+type Group struct {
+    ID           	uint  			`gorm:"primarykey"`
+    Name         	string 			`gorm:"not null"`
+    Description  	string
+	Users        	[]User			`gorm:"constraint:OnDelete:SET NULL"`
+    Subgroups    	[]Subgroup		`gorm:"constraint:OnDelete:CASCADE"`
+    Announcements	[]Announcement	`gorm:"constraint:OnDelete:CASCADE"`
+    Events       	[]Event			`gorm:"constraint:OnDelete:CASCADE"`
+    Tracks       	[]Track			`gorm:"constraint:OnDelete:CASCADE"`
+}
