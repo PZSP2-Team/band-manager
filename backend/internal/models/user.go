20e562effa3d0977fc3dcc6a248bb068af129af1
mrozek <01178582@pw.edu.pl>
2024-12-08 18:24:49 +0100

    FIX: Changed role field in user model so it can be null

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	LastName      string `gorm:"not null"`
 	Email         string `gorm:"unique;not null"`
 	PasswordHash  string `gorm:"not null"`
-	Role          string `gorm:"not null"`
+	Role          string 
 	GroupID       uint
 	Announcements []Announcement `gorm:"foreignKey:SenderID;constraint:OnDelete:SET NULL"`
 	Subgroups     []*Subgroup    `gorm:"many2many:subgroup_user;constraint:OnDelete:CASCADE"`
