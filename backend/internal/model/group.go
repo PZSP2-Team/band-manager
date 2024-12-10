8d47c27caf7267620961c960713357ef9719907c
mrozek <01178582@pw.edu.pl>
2024-12-10 19:18:15 +0100

    FIX: Removed unique constraint from name

--- backend/internal/model/group.go
+++ backend/internal/model/group.go
@@ -2,7 +2,7 @@ package model
 
 type Group struct {
 	ID            uint   `gorm:"primarykey"`
-	Name          string `gorm:"unique;not null"`
+	Name          string `gorm:"not null"`
 	AccessToken	  string `gorm:"unique;not null"`
 	Description   string
 	Users         []User         `gorm:"constraint:OnDelete:SET NULL"`
