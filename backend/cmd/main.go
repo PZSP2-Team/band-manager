077f7d1f045f6d4c0b2bc8932906947cf171d95f
Marcin Lisowski <mnlisows@gmail.com>
2024-12-10 07:03:25 +0100

    register router initialization

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"band-manager-backend/internal/handlers"
 )
 
 func main() {
@@ -25,4 +26,10 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+
+	// Inicjalizacja handlerów (kontrolerów)
+	userHandler := handlers.NewUserHandler(database)
+
+	// Routing
+	http.HandleFunc("/register", userHandler.Register)
 }
