c5efbe534f94af8f6b771ec0545c6faca47c52eb
Marcin Lisowski <mnlisows@gmail.com>
2024-12-11 12:16:53 +0100

    repo - GetUserByEmail

--- backend/internal/repositories/userRepository.go
+++ backend/internal/repositories/userRepository.go
@@ -23,3 +23,15 @@ func NewUserRepository() *UserRepository {
 		db: db.GetDB(), // Używamy funkcji GetDB() z pakietu db do uzyskania instancji połączenia z bazą danych
 	}
 }
+
+// GetUserByEmail pobiera użytkownika na podstawie adresu email
+func (r *UserRepository) GetUserByEmail(email string) (*model.User, error) {
+	var user model.User
+	// Używamy GORM do wyszukiwania użytkownika po emailu
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		// Jeśli nie znaleziono użytkownika, zwracamy błąd
+		return nil, errors.New("user not found")
+	}
+	// Zwracamy użytkownika
+	return &user, nil
+}
