3c14e95ec81cc2651196299fa883a1225fa8bf38
mrozek <01178582@pw.edu.pl>
2024-12-11 21:34:43 +0100

    IMPLEMENT: Implemented proxy pass to backend url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/api/auth/:auth*",
+                destination: "/api/auth/:auth*"
+            },
+            {
+                source: "/api/:path*",
+                destination: `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/:path*`
+            }
+        ]
+    }
 };
 
 export default nextConfig;
