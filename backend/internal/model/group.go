62c25490369dafc1385c6679d6a000c3d0b58516
Marcin Lisowski <mnlisows@gmail.com>
2025-01-07 20:48:55 +0100

    FIX - Database changes to enable user to belong to multiple groups

--- backend/internal/model/group.go
+++ backend/internal/model/group.go
@@ -3,9 +3,9 @@ package model
 type Group struct {
 	ID            uint   `gorm:"primarykey"`
 	Name          string `gorm:"not null"`
-	AccessToken	  string `gorm:"unique;not null"`
+	AccessToken   string `gorm:"unique;not null"`
 	Description   string
-	Users         []User         `gorm:"constraint:OnDelete:SET NULL"`
+	Users         []*User        `gorm:"many2many:user_group;constraint:OnDelete:CASCADE"`
 	Subgroups     []Subgroup     `gorm:"constraint:OnDelete:CASCADE"`
 	Announcements []Announcement `gorm:"constraint:OnDelete:CASCADE"`
 	Events        []Event        `gorm:"constraint:OnDelete:CASCADE"`
