1addf555787f4a744b8ea05a86a863064b315796
mrozek <01178582@pw.edu.pl>
2025-01-08 22:54:41 +0100

    REFACTOR: Removed unnecessary fields from session user interface

--- frontend/types/next-auth.d.ts
+++ frontend/types/next-auth.d.ts
@@ -4,15 +4,11 @@ import { DefaultSession } from "next-auth";
 declare module 'next-auth' {
     interface User {
         id?: number | null;
-        role?: string | null;
-        groupId?: number | null;
     }
 
     interface Session {
         user: {
             id?: number | null;
-            role?: string | null;
-            groupId?: number | null;
         } & DefaultSession["user"];
     }
 }
