2be83b97b9a7c29a1316043d364b7b2639f86047
Marcin Lisowski <mnlisows@gmail.com>
2025-01-08 19:36:06 +0100

    FIX: usergrouprole table missing connections to user and group
    Co-authored-by: Maks Bilski <maksde25@gmail.com>

--- backend/internal/model/usergrouprole.go
+++ backend/internal/model/usergrouprole.go
@@ -4,4 +4,8 @@ type UserGroupRole struct {
 	UserID  uint   `gorm:"primarykey"`
 	GroupID uint   `gorm:"primarykey"`
 	Role    string `gorm:"not null"` // "manager", "moderator", "member"
+
+	// Dodanie relacji
+	User  User  `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	Group Group `gorm:"foreignKey:GroupID;constraint:OnDelete:CASCADE"`
 }
