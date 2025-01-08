13e40dff5542ec8116c4fa4cb72ba23bacb2cb9c
Marcin Lisowski <mnlisows@gmail.com>
2025-01-08 14:38:57 +0100

    FIX: database changes to include multiple groups within single user

--- backend/internal/model/group.go
+++ backend/internal/model/group.go
@@ -5,9 +5,10 @@ type Group struct {
 	Name          string `gorm:"not null"`
 	AccessToken   string `gorm:"unique;not null"`
 	Description   string
-	Users         []*User        `gorm:"many2many:user_group;constraint:OnDelete:CASCADE"`
-	Subgroups     []Subgroup     `gorm:"constraint:OnDelete:CASCADE"`
-	Announcements []Announcement `gorm:"constraint:OnDelete:CASCADE"`
-	Events        []Event        `gorm:"constraint:OnDelete:CASCADE"`
-	Tracks        []Track        `gorm:"constraint:OnDelete:CASCADE"`
+	Users         []*User         `gorm:"many2many:user_group;constraint:OnDelete:CASCADE"`
+	Subgroups     []Subgroup      `gorm:"constraint:OnDelete:CASCADE"`
+	Announcements []Announcement  `gorm:"constraint:OnDelete:CASCADE"`
+	Events        []Event         `gorm:"constraint:OnDelete:CASCADE"`
+	Tracks        []Track         `gorm:"constraint:OnDelete:CASCADE"`
+	UserRoles     []UserGroupRole `gorm:"foreignKey:GroupID;constraint:OnDelete:CASCADE"`
 }
--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -1,13 +1,15 @@
 package model
 
 type User struct {
-	ID            uint   `gorm:"primarykey"`
-	FirstName     string `gorm:"not null"`
-	LastName      string `gorm:"not null"`
-	Email         string `gorm:"unique;not null"`
-	PasswordHash  string `gorm:"not null"`
-	Role          string
-	GroupID       *uint
-	Announcements []Announcement `gorm:"foreignKey:SenderID;constraint:OnDelete:SET NULL"`
-	Subgroups     []*Subgroup    `gorm:"many2many:subgroup_user;constraint:OnDelete:CASCADE"`
+	ID           uint   `gorm:"primarykey"`
+	FirstName    string `gorm:"not null"`
+	LastName     string `gorm:"not null"`
+	Email        string `gorm:"unique;not null"`
+	PasswordHash string `gorm:"not null"`
+	// Role         string
+	// GroupID       *uint
+	Groups        []*Group        `gorm:"many2many:user_group;constraint:OnDelete:CASCADE"`
+	Announcements []Announcement  `gorm:"foreignKey:SenderID;constraint:OnDelete:SET NULL"`
+	Subgroups     []*Subgroup     `gorm:"many2many:subgroup_user;constraint:OnDelete:CASCADE"`
+	GroupRoles    []UserGroupRole `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
