508bc18463e1e7d3a213c63a1a2fee1a64c02b79
mrozek <01178582@pw.edu.pl>
2025-01-08 22:53:47 +0100

    ADD: Added Sidebar to dashboard layout

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -1,14 +1,21 @@
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 export default function ProtectedLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+
     return (
-        <>
+        <div className="h-screen flex flex-col">
             <Header />
-            <main>{children}</main>
-        </>
+            <div className="flex-1 flex h-[calc(100vh-74px)]">
+                <Sidebar/>
+                <main className="flex-1 p-8">
+                    {children}
+                </main>
+            </div>
+        </div>
     );
 }
