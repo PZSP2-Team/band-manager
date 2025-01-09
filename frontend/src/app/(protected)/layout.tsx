f1f46c634080f9b2c5b93bfa68dfdb7d5c7d39da
mrozek <01178582@pw.edu.pl>
2025-01-09 19:59:45 +0100

    IMPLEMENT: Added NavigationBar

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -1,24 +1,25 @@
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import Header from "@/src/app/components/Header";
+import Sidebar from "@/src/app/components/Sidebar";
+import NavigationBar from "@/src/app/components/Navigation";
 import { GroupProvider } from "../contexts/GroupContext";
 
 export default function ProtectedLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-    return (
-        <div className="h-screen flex flex-col">
-            <Header />
-            <div className="flex-1 flex h-[calc(100vh-74px)]">
-                <GroupProvider>
-                    <Sidebar/>
-                    <main className="flex-1 p-8">
-                        {children}
-                    </main>
-                </GroupProvider>
-            </div>
-        </div>
-    );
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className="flex-1 flex h-[calc(100vh-1200px)]">
+        <GroupProvider>
+          <Sidebar />
+          <div className="flex flex-col w-full">
+            <NavigationBar />
+            <main className="flex-1 overflow-y-auto">{children}</main>
+          </div>
+        </GroupProvider>
+      </div>
+    </div>
+  );
 }
