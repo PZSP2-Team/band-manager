cabcc6c66183948ee630b5550e83bc64565dd740
mrozek <01178582@pw.edu.pl>
2025-01-09 19:56:22 +0100

    REFACTOR: Changed url to dashboard and formatted with prettier

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,27 +4,29 @@ import { NextResponse } from "next/server";
 export default withAuth(
   function middleware(req) {
     const path = req.nextUrl.pathname;
-    const isPublicPath = path === "/login" || path === "/register" || path === "/";
+    const isPublicPath =
+      path === "/login" || path === "/register" || path === "/";
 
     if (isPublicPath && req.nextauth.token) {
-      return NextResponse.redirect(new URL("/groups", req.url));
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
   },
   {
     callbacks: {
       authorized: ({ token, req }) => {
         const path = req.nextUrl.pathname;
-        const isPublicPath = path === "/login" || path === "/register" || path === "/";
-        
+        const isPublicPath =
+          path === "/login" || path === "/register" || path === "/";
+
         if (isPublicPath) {
           return true;
         }
         return !!token;
       },
     },
-  }
+  },
 );
 
 export const config = {
-  matcher: ["/", "/login", "/register", "/groups/:path*", "/events/:path*"],
+  matcher: ["/", "/login", "/register", "/dashboard/:path*", "/events/:path*"],
 };
