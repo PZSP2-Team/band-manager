6729561eb55931568a3867a985adedfa299a9b9c
Marcin Lisowski <mnlisows@gmail.com>
2025-01-09 02:25:51 +0100

    FIX: Instrument field included in Notesheet model
    Co-authored-by: Maks Bilski <maksde25@gmail.com>

--- backend/internal/model/notesheet.go
+++ backend/internal/model/notesheet.go
@@ -1,8 +1,9 @@
 package model
 
 type Notesheet struct {
-	ID        uint        `gorm:"primarykey"`
-	Filepath  string      `gorm:"not null"`
-	TrackId   uint        `gorm:"not null;"`
-	Subgroups []*Subgroup `gorm:"many2many:notesheet_subgroup;constraint:OnDelete:CASCADE"`
+	ID         uint        `gorm:"primarykey"`
+	Filepath   string      `gorm:"not null"`
+	Instrument string      `gorm:"not null"`
+	TrackId    uint        `gorm:"not null;"`
+	Subgroups  []*Subgroup `gorm:"many2many:notesheet_subgroup;constraint:OnDelete:CASCADE"`
 }
