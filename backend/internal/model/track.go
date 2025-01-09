f1012f46ab839c918e2e7936104e2b726b3ac65a
Marcin Lisowski <mnlisows@gmail.com>
2025-01-09 02:23:15 +0100

    FIX: Description field added to Track model

--- backend/internal/model/track.go
+++ backend/internal/model/track.go
@@ -1,10 +1,11 @@
 package model
 
 type Track struct {
-	ID         uint        `gorm:"primarykey"`
-	Name       string      `gorm:"not null"`
-	GroupID    uint        `gorm:"not null"`
-	Events     []*Event    `gorm:"many2many:track_event;constraint:OnDelete:CASCADE"`
-	Notesheets []Notesheet `gorm:"constraint:OnDelete:CASCADE"`
+	ID           uint          `gorm:"primarykey"`
+	Name         string        `gorm:"not null"`
+	GroupID      uint          `gorm:"not null"`
+	Description  string        `gorm:"not null"`
+	Events       []*Event      `gorm:"many2many:track_event;constraint:OnDelete:CASCADE"`
+	Notesheets   []Notesheet   `gorm:"constraint:OnDelete:CASCADE"`
 	Performances []Performance `gorm:"constraint:OnDelete:CASCADE"`
 }
