7ce1adad0b174040af514d0b4f0466ac3c7de6ea
Maksymilian Bilski <maksde25@gmail.com>
2025-01-13 00:33:12 +0100

    feat: add Google Calendar token model for storing OAuth credentials

--- backend/internal/model/googlecalendartoken.go
+++ backend/internal/model/googlecalendartoken.go
@@ -1,1 +1,12 @@
 package model
+
+import "time"
+
+type GoogleCalendarToken struct {
+	ID           uint      `gorm:"primarykey" json:"id"`
+	UserID       uint      `gorm:"not null;uniqueIndex" json:"user_id"`
+	AccessToken  string    `gorm:"not null" json:"access_token"`
+	RefreshToken string    `gorm:"not null" json:"refresh_token"`
+	ExpiryTime   time.Time `gorm:"not null" json:"expiry_time"`
+	User         User      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"-"`
+}
