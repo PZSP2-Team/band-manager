50db6958fdd774535e2d4de2db33de0abb1ed41c
Maksymilian Bilski <maksymilian.bilski@nokia.com>
2025-01-12 12:48:51 +0100

    Break down CreateEvent and UpdateEvent methods into smaller, reusable private functions to improve code readability and maintainability. Extract common validation and business logic into separate functions to reduce code duplication.

--- backend/internal/usecases/EventUsecase.go
+++ backend/internal/usecases/EventUsecase.go
@@ -4,6 +4,7 @@ import (
 	"band-manager-backend/internal/model"
 	"band-manager-backend/internal/repositories"
 	"band-manager-backend/internal/services"
+	"band-manager-backend/internal/usecases/helpers"
 	"errors"
 	"log"
 	"time"
@@ -28,15 +29,11 @@ func NewEventUsecase(gcService *services.GoogleCalendarService, emailService *se
 		emailService: emailService,
 	}
 }
-
 func (u *EventUsecase) CreateEvent(title, description, location string, date time.Time,
 	groupID uint, trackIDs []uint, userIDs []uint, userID uint) (*model.Event, error) {
-	role, err := u.groupRepo.GetUserRole(userID, groupID)
-	if err != nil {
-		return nil, errors.New("user not in group")
-	}
-	if role != "manager" && role != "moderator" {
-		return nil, errors.New("insufficient permissions")
+
+	if err := u.validateUserPermissions(userID, groupID); err != nil {
+		return nil, err
 	}
 
 	event := &model.Event{
@@ -51,57 +48,91 @@ func (u *EventUsecase) CreateEvent(title, description, location string, date tim
 		return nil, err
 	}
 
-	if len(trackIDs) > 0 {
-		for _, trackID := range trackIDs {
-			track, err := u.trackRepo.GetTrackByID(trackID)
-			if err != nil {
-				return event, err
-			}
-			if track.GroupID != groupID {
-				return event, errors.New("track does not belong to this group")
-			}
-		}
-		err = u.eventRepo.AddTracksToEvent(event.ID, trackIDs)
+	if err := u.addTracksToEvent(event, trackIDs); err != nil {
+		return event, err
+	}
+
+	if err := u.addUsersToEvent(event, userIDs); err != nil {
+		return event, err
+	}
+
+	u.handleExternalIntegrations(event, userIDs)
+
+	return event, nil
+}
+
+func (u *EventUsecase) validateUserPermissions(userID, groupID uint) error {
+	role, err := u.groupRepo.GetUserRole(userID, groupID)
+	if err != nil {
+		return errors.New("user not in group")
+	}
+	if !helpers.HasManagerOrModeratorRole(role) {
+		return errors.New("insufficient permissions")
+	}
+	return nil
+}
+
+func (u *EventUsecase) addTracksToEvent(event *model.Event, trackIDs []uint) error {
+	if len(trackIDs) == 0 {
+		return nil
+	}
+
+	for _, trackID := range trackIDs {
+		track, err := u.trackRepo.GetTrackByID(trackID)
 		if err != nil {
-			return event, err
+			return errors.New("Could not get track by ID")
+		}
+		if track.GroupID != event.GroupID {
+			return errors.New("track does not belong to this group")
 		}
 	}
 
+	return u.eventRepo.AddTracksToEvent(event.ID, trackIDs)
+}
+
+func (u *EventUsecase) addUsersToEvent(event *model.Event, userIDs []uint) error {
 	if len(userIDs) > 0 {
 		for _, assignedUserID := range userIDs {
-			_, err := u.groupRepo.GetUserRole(assignedUserID, groupID)
+			_, err := u.groupRepo.GetUserRole(assignedUserID, event.GroupID)
 			if err != nil {
-				return event, errors.New("some users are not in the group")
+				return errors.New("some users are not in the group")
 			}
 		}
-		err = u.eventRepo.AddUsersToEvent(event.ID, userIDs)
-		if err != nil {
-			return event, err
-		}
-	} else {
-
-		groupUsers, err := u.groupRepo.GetGroupMembers(groupID)
-		if err != nil {
-			return event, err
-		}
-		var allUserIDs []uint
-		for _, user := range groupUsers {
-			allUserIDs = append(allUserIDs, user.ID)
-		}
-		err = u.eventRepo.AddUsersToEvent(event.ID, allUserIDs)
-		if err != nil {
-			return event, err
-		}
+		return u.eventRepo.AddUsersToEvent(event.ID, userIDs)
 	}
 
+	groupUsers, err := u.groupRepo.GetGroupMembers(event.GroupID)
+	if err != nil {
+		return err
+	}
+	var allUserIDs []uint
+	for _, user := range groupUsers {
+		allUserIDs = append(allUserIDs, user.ID)
+	}
+	return u.eventRepo.AddUsersToEvent(event.ID, allUserIDs)
+}
+
+func (u *EventUsecase) handleExternalIntegrations(event *model.Event, userIDs []uint) {
 	if u.gcService != nil {
 		if err := u.gcService.CreateCalendarEvent(event); err != nil {
 			log.Printf("Failed to sync with Google Calendar: %v", err)
 		}
 	}
-	var recipients []*model.User
 
+	recipients, err := u.getEventRecipients(event.GroupID, userIDs)
+	if err != nil {
+		log.Printf("Failed to get recipients: %v", err)
+		return
+	}
+
+	if len(recipients) > 0 {
+		go u.emailService.SendEventEmail(event, recipients)
+	}
+}
+
+func (u *EventUsecase) getEventRecipients(groupID uint, userIDs []uint) ([]*model.User, error) {
 	if len(userIDs) > 0 {
+		var recipients []*model.User
 		for _, id := range userIDs {
 			user, err := u.userRepo.GetUserByID(id)
 			if err != nil {
@@ -109,18 +140,10 @@ func (u *EventUsecase) CreateEvent(title, description, location string, date tim
 			}
 			recipients = append(recipients, user)
 		}
-	} else {
-		recipients, err = u.groupRepo.GetGroupMembers(groupID)
-		if err != nil {
-			log.Printf("Failed to get group members: %v", err)
-		}
+		return recipients, nil
 	}
 
-	if len(recipients) > 0 {
-		go u.emailService.SendEventEmail(event, recipients)
-	}
-
-	return event, nil
+	return u.groupRepo.GetGroupMembers(groupID)
 }
 
 func (u *EventUsecase) GetEvent(eventID uint, userID uint) (*model.Event, error) {
@@ -131,7 +154,7 @@ func (u *EventUsecase) GetEvent(eventID uint, userID uint) (*model.Event, error)
 
 	_, err = u.groupRepo.GetUserRole(userID, event.GroupID)
 	if err != nil {
-		return nil, errors.New("access denied")
+		return nil, errors.New("User not in group")
 	}
 
 	return event, nil
@@ -139,89 +162,52 @@ func (u *EventUsecase) GetEvent(eventID uint, userID uint) (*model.Event, error)
 
 func (u *EventUsecase) UpdateEvent(id uint, title, description, location string,
 	date time.Time, trackIDs []uint, userIDs []uint, userID uint) error {
+
 	event, err := u.eventRepo.GetEventByID(id)
 	if err != nil {
 		return err
 	}
 
-	role, err := u.groupRepo.GetUserRole(userID, event.GroupID)
-	if err != nil {
-		return errors.New("access denied")
-	}
-	if role != "manager" && role != "moderator" {
-		return errors.New("insufficient permissions")
+	if err := u.validateUserPermissions(userID, event.GroupID); err != nil {
+		return err
 	}
 
-	event.Title = title
-	event.Description = description
-	event.Location = location
-	event.Date = date
-
-	if err := u.eventRepo.UpdateEvent(event); err != nil {
+	if err := u.updateEventBasicInfo(event, title, description, location, date); err != nil {
 		return err
 	}
 
 	if trackIDs != nil {
-		for _, trackID := range trackIDs {
-			track, err := u.trackRepo.GetTrackByID(trackID)
-			if err != nil {
-				return err
-			}
-			if track.GroupID != event.GroupID {
-				return errors.New("track does not belong to this group")
-			}
-		}
-		err = u.eventRepo.AddTracksToEvent(event.ID, trackIDs)
-		if err != nil {
+		if err := u.addTracksToEvent(event, trackIDs); err != nil {
 			return err
 		}
 	}
 
 	if userIDs != nil {
-		if len(userIDs) > 0 {
-
-			for _, assignedUserID := range userIDs {
-				_, err := u.groupRepo.GetUserRole(assignedUserID, event.GroupID)
-				if err != nil {
-					return errors.New("some users are not in the group")
-				}
-			}
-			err = u.eventRepo.AddUsersToEvent(event.ID, userIDs)
-			if err != nil {
-				return err
-			}
-		} else {
-
-			groupUsers, err := u.groupRepo.GetGroupMembers(event.GroupID)
-			if err != nil {
-				return err
-			}
-			var allUserIDs []uint
-			for _, user := range groupUsers {
-				allUserIDs = append(allUserIDs, user.ID)
-			}
-			err = u.eventRepo.AddUsersToEvent(event.ID, allUserIDs)
-			if err != nil {
-				return err
-			}
+		if err := u.addUsersToEvent(event, userIDs); err != nil {
+			return err
 		}
 	}
 
 	return nil
 }
+
+func (u *EventUsecase) updateEventBasicInfo(event *model.Event, title, description, location string, date time.Time) error {
+	event.Title = title
+	event.Description = description
+	event.Location = location
+	event.Date = date
+
+	return u.eventRepo.UpdateEvent(event)
+}
+
 func (u *EventUsecase) DeleteEvent(id uint, userID uint) error {
 	event, err := u.eventRepo.GetEventByID(id)
 	if err != nil {
 		return err
 	}
 
-	role, err := u.groupRepo.GetUserRole(userID, event.GroupID)
-	if err != nil {
-		return errors.New("access denied")
-	}
-
-	if role != "manager" && role != "moderator" {
-		return errors.New("insufficient permissions")
+	if err := u.validateUserPermissions(userID, event.GroupID); err != nil {
+		return err
 	}
 
 	return u.eventRepo.DeleteEvent(id)
@@ -230,7 +216,7 @@ func (u *EventUsecase) DeleteEvent(id uint, userID uint) error {
 func (u *EventUsecase) GetGroupEvents(groupID uint, userID uint) ([]*model.Event, error) {
 	_, err := u.groupRepo.GetUserRole(userID, groupID)
 	if err != nil {
-		return nil, errors.New("access denied")
+		return nil, errors.New("User not in group")
 	}
 
 	return u.eventRepo.GetGroupEvents(groupID)
@@ -248,7 +234,7 @@ func (u *EventUsecase) GetEventTracks(eventID uint, userID uint) ([]*model.Track
 
 	_, err = u.groupRepo.GetUserRole(userID, event.GroupID)
 	if err != nil {
-		return nil, errors.New("access denied")
+		return nil, errors.New("User not in group")
 	}
 
 	return u.eventRepo.GetEventTracks(eventID)
